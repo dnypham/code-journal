74cbc53535250b17fa9eca4668a78c6a8a48a0af
Daniel Pham <dnypham@gmail.com>
2021-09-08 10:48:16 -0700

    Listen for submit event on journal entry form. #1

--- js/main.js
+++ js/main.js
@@ -7,4 +7,21 @@ $form.addEventListener('input', function (event) {
   if (event.target.matches('.photo-url')) {
     $img.setAttribute('src', event.target.value);
   }
+  if (event.target.value === '') {
+    $img.setAttribute('src', 'images/placeholder-image-square.jpg');
+  }
+});
+
+$form.addEventListener('submit', function (event) {
+  event.preventDefault();
+  var entry = {
+    title: $form.title.value,
+    photoUrl: $form.photoUrl.value,
+    notes: $form.notes.value
+  };
+
+  data.nextEntryId++;
+  data.entries.unshift(entry);
+  $img.setAttribute('src', 'images/placeholder-image-square.jpg');
+  $form.reset();
 });
